8476a83aa759e7b1650b6f2748fe3b6d43e556be
MENGHAYYU <81160542+Jbot55@users.noreply.github.com>
2022-05-31 17:10:05 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['62895605030379','62895605030379','62895605030379'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['62895605030379'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '62895605030379' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = '𝐉 𝐔 𝐍' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'JUNN BOT' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = '𝐉 𝐔 𝐍' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
@@ -41,8 +41,8 @@ global.email = 'riychdwaynee@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© JUNN-BOT' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = '𝐉 𝐔 𝐍' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
